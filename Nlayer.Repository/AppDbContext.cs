efa661b81b60ad67e56e449ddb76a96eddb00ce5
cenikfth <cenikfth@gmail.com>
2023-09-10 19:30:54 +0300

    Entity Configurations Eklendi

--- Nlayer.Repository/AppDbContext.cs
+++ Nlayer.Repository/AppDbContext.cs
@@ -13,5 +13,12 @@ namespace Nlayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasKey(c => c.Id);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
