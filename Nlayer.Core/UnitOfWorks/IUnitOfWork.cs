2737de40072f44d24e2348b7e1e43cddd2c1909d
cenikfth <cenikfth@gmail.com>
2023-09-10 23:15:38 +0300

    GenericRepository Oluşturuldu

--- Nlayer.Core/UnitOfWorks/IUnitOfWork.cs
+++ Nlayer.Core/UnitOfWorks/IUnitOfWork.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Nlayer.Core.UnitOfWorks
 {
-    internal interface IUnitOfWork
+    public interface IUnitOfWork
     {
+        Task CommitAsync();
+        void Comit();
     }
 }
