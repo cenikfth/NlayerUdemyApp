dd825f550f192cc80b3e5845d778d5ea036842e2
cenikfth <cenikfth@gmail.com>
2023-09-23 13:51:12 +0300

    ProductWithCategory Endpoint created

--- Nlayer.API/Controllers/ProductController.cs
+++ Nlayer.API/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Nlayer.Core.DTOs;
 using Nlayer.Core.Models;
 using Nlayer.Core.Services;
+using Nlayer.Service.Services;
 
 namespace Nlayer.API.Controllers
 {
@@ -12,17 +13,64 @@ namespace Nlayer.API.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IService<Product> _service;
+        private readonly IProductService _productService;
 
-        public ProductController(IService<Product> service, IMapper mapper)
+        public ProductController(IService<Product> service, IMapper mapper, IProductService productService)
         {
             _service = service;
             _mapper = mapper;
+            _productService = productService;
         }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetProductsWithCategory() 
+        {
+            return CreateActionResult(await _productService.GetProductWithCategory());
+        
+        }
+
+
+        [HttpGet]
         public async Task<IActionResult> All()
         {
             var products = await _service.GetAllAsync();
             var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
-            return Ok(CustomResponseDto<List<ProductDto>>.Success(200,productsDtos));
+
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            var productDto = _mapper.Map<ProductDto>(product);
+
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productDto));
+        }
+        
+        [HttpPost]       
+               
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var productsDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDto));
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+         
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(product);
+          
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
         }
     }
 }
---
+++ Nlayer.Service/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Nlayer.Core.DTOs;
+using Nlayer.Core.Models;
+using Nlayer.Core.Repositories;
+using Nlayer.Core.Services;
+using Nlayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nlayer.Service.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+
+        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductWithCategory()
+        {
+            var product = await _productRepository.GetProductWithCategory();
+            var productsDto= _mapper.Map<List<ProductWithCategoryDto>>(product);
+            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsDto);
+        }
+    }
+}
